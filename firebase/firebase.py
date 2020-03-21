863070dd827cea6c0d9ce1d117b46528fd5264c1
Futaba-Kosuke <k.futaba2828@gmail.com>
2020-03-21 21:54:19 +0900

    Add: アサイン機能の実装

--- firebase/firebase.py
+++ firebase/firebase.py
@@ -2,9 +2,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+import datetime
+
 class FireBase:
 
-    def __init__(self, path, team_name):
+    def __init__(self, path, team_name, channel=None, repository=None):
 
         cred = credentials.Certificate(path)
         firebase_admin.initialize_app(cred)
@@ -12,24 +14,104 @@ class FireBase:
 
         self.team_name = team_name
 
-    def get_all_issues(self):
-        return self.db.collection(self.team_name).document(u'issues').get().to_dict()
+        # summaryが未定義のとき、collectionが存在しない
+        summary = self.db.collection(self.team_name).document(u'summary').get().to_dict()
+        if summary == None:
+            print('init_collection')
+            collection = self.db.collection(self.team_name)
+            collection.document(u'summary').set({
+                u'channel': channel,
+                u'repository': repository,
+            })
+            collection.document(u'users').set({})
+            collection.document(u'issues').set({})
 
-    def get_all_users(self):
-        return self.db.collection(self.team_name).document(u'users').get().to_dict()
+    def get_issues_ref(self):
+        return self.db.collection(self.team_name).document(u'issues')
+
+    def get_users_ref(self):
+        return self.db.collection(self.team_name).document(u'users')
 
     def get_issue(self, num):
-        return self.get_all_issues()[num]
+        return self.get_issues_ref().get().to_dict()[num]
 
     def get_user(self, slack_id):
-        return self.get_all_users()[slack_id]
+        return self.get_users_ref().get().to_dict()[slack_id]
 
-path = 'testAccount.json'
-team_name = 'Team01'
+    def add_issue(self, issue_num, title, description, limited_sec, closed_at=None, assignee=[]):
+        issues_ref = self.get_issues_ref()
+        issues = issues_ref.get().to_dict()
+        if len(assignee) == 0:
+            assigned_at = None
+        else:
+            assigned_at = datetime.datetime.now()
 
-firebase = FireBase(path, team_name)
+        new_issue = {
+            u'issue_num': str(issue_num),
+            u'title': title,
+            u'description': description,
+            u'assignee': assignee,
+            u'limited_sec': limited_sec,
+            u'assigned_at': assigned_at,
+            u'closed_at': closed_at,
+        }
+        issues[str(issue_num)] = new_issue
+        issues_ref.set(issues)
+    
+    def add_user(self, slack_id, github_id):
+        users_ref = self.get_users_ref()
+        users = users_ref.get().to_dict()
+        new_user = {
+            u'slack_id': slack_id,
+            u'github_id': github_id,
+            u'issues': [],
+        }
+        users[slack_id] = new_user
+        users_ref.set(users)
+    
+    def assign_user(self, slack_id, issue_num):
+        issues_ref = self.get_issues_ref()
+        users_ref = self.get_users_ref()
 
-print(firebase.get_all_issues())
-print(firebase.get_all_users())
-print(firebase.get_issue(str(1)))
-print(firebase.get_user('U010AV12F8D'))
+        issues = issues_ref.get().to_dict()
+        users = users_ref.get().to_dict()
+
+        issues[str(issue_num)][u'assignee'].append(slack_id)
+        issues[str(issue_num)][u'assigned_at'] = datetime.datetime.now()
+        users[slack_id][u'issues'].append(issue_num)
+
+        issues_ref.set(issues)
+        users_ref.set(users)
+
+    def close_issue(self, issue_num):
+        issues_ref = self.get_issues_ref()
+
+        issues = issues_ref.get().to_dict()
+
+        issues[str(issue_num)][u'closed_at'] = datetime.datetime.now()
+
+        issues_ref.set(issues)
+
+path = u'testAccount.json'
+team_name = u'Team02'
+channel = {
+    u'signing_secret': u'signing_secret',
+    u'token': u'token',
+}
+repository = u'repository'
+
+firebase = FireBase(path, team_name, channel, repository)
+
+firebase.add_user('slack', 'github')
+firebase.add_issue(1, 'title', 'description', 100, None, [])
+firebase.assign_user('slack', 1)
+firebase.close_issue(1)
+
+# {
+#     u'number': str(number),
+#     u'title': title,
+#     u'description': description,
+#     u'assignee': assignee,
+#     u'limited_sec': limited_time,
+#     u'closed_at': closed_at,
+# }
