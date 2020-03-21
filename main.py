57d0eda1d937b26790eeccd923698aaf8ed4776c
rkun <rkun123@hotmail.com>
2020-03-22 06:17:35 +0900

    Wip: main.py

--- main.py
+++ main.py
@@ -27,17 +27,48 @@ if __name__ == "__main__":
     github = Issue(GITHUB_ACCESS_TOKEN)
     firebase = FireBase(join(dirname(__file__), FIREBASE_CREDENTIAL_PATH))
 
-    def addIssue(team_id, title, description, selected_users):
-        print(title)
-        print(description)
-        print(selected_users)
-
     # Generate slack client and run
     slack = Slack(
         SLACKBOT_API_TOKEN,
         SLACKBOT_API_SIGNING_SECRET,
         flask,
-        3000,
-        addIssue
+        3000
     )
+
+    def addIssue(team_id, title, description, limited_time, selected_users):
+        """
+        Issue追加
+        """
+        github.create_issue()
+        print(title)
+        print(description)
+        print(selected_users)
+
+    slack.setAddIssueCallback(addIssue)
+
+    def addChannel(team_id, repo):
+        """
+        Channel追加
+        """
+        firebase.add_channel(team_id, repo)
+
+    slack.setAddChannelCallback(addIssue)
+
+    def assignUser(team_id, assignee, issue_num):
+        """
+        ユーザーアサイン
+        """
+        github.get_issue(
+
+    slack.setAssignUserCallback(addIssue)
+
+    def closeIssue(team_id, issue_num):
+        """
+        Issueクローズ
+        """
+        github.close_issue(repo, issue_num)
+        
+
+    slack.setAssignUserCallback(addIssue)
+
     slack.run()
