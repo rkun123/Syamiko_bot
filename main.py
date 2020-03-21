689fa0850412398c35bbdcc15e123ffbdf0bac3a
rkun <rkun123@hotmail.com>
2020-03-22 02:17:23 +0900

    FireBase import bugfix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 from slack_api import Slack
 from github_issue import Issue
-from firebase.firebase import Firebase
+from firebase.firebase import FireBase
 
 if __name__ == "__main__":
     # Load dotenv
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     SLACKBOT_API_TOKEN = os.environ.get("SLACKBOT_API_TOKEN")
     SLACKBOT_API_SIGNING_SECRET = os.environ.get("SLACKBOT_API_SIGNING_SECRET")
-    
+
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
 
     FIREBASE_CREDENTIAL_PATH = os.environ.get("FIREBASE_CREDENTIAL_PATH")
@@ -25,12 +25,12 @@ if __name__ == "__main__":
 
     # Generate github client
     github = Issue(GITHUB_ACCESS_TOKEN)
-    firebase = Firebase(join(dirname(__file__), '.env'))
+    firebase = FireBase(join(dirname(__file__), FIREBASE_CREDENTIAL_PATH))
 
     def addIssue(team_id, title, description, selected_users):
-        github.create_issue(
-            , title, description, assignee=selected_users[0])
-        firebase.add_issue(team_id, _, title, description, _, selected_users)
+        print(title)
+        print(description)
+        print(selected_users)
 
     # Generate slack client and run
     slack = Slack(
